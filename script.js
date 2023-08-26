87337838b342fb42d7324050d518ad3f96a56744
ZhuonengBao <zhuoneng010@outlook.com>
2023-08-26 13:50:25 +0200

    Progress in the cpu-loading

--- script.js
+++ script.js
@@ -12,3 +12,21 @@ function goTopFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+function move() {
+  var elem = document.getElementById("myBar");   
+  var width = 0;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  move();
+});
