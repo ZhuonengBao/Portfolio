4f1fd32f44a690a2b0e9744d6353e9605b1e77ea
ZhuonengBao <zhuoneng010@outlook.com>
2023-08-28 16:04:38 +0200

    Progression in cpulines

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("navbar");
     if (x.className === "navbar") {
@@ -13,20 +12,57 @@ function goTopFunction() {
   document.documentElement.scrollTop = 0;
 }
 
-function move() {
-  var elem = document.getElementById("myBar");   
+function move(elementId, maxWidth) {
+  var elem = document.getElementById(elementId);
   var width = 0;
   var id = setInterval(frame, 10);
+  
   function frame() {
-    if (width >= 100) {
+    if (width >= maxWidth) {
       clearInterval(id);
     } else {
-      width++; 
-      elem.style.width = width + '%'; 
+      width++;
+      elem.style.width = width + '%';
     }
   }
 }
 
+function createCircularProgress() {
+  const circularProgress = document.querySelector(".circular-progress");
+
+  let startValue = 0,
+    endValue = 100,
+    speed = 10,
+    progressColor = "black";
+
+  const progress = setInterval(() => {
+    startValue++;
+    circularProgress.style.background = `conic-gradient(${progressColor} ${
+      (startValue * 360) / 100
+    }deg, transparent ${(startValue * 360) / 100}deg)`;
+
+    if (startValue === endValue) {
+      clearInterval(progress);
+    }
+  }, speed);
+}
+
 document.addEventListener("DOMContentLoaded", function() {
-  move();
+  var elementsToAnimate1 = ["myBar4"];
+  var elementsToAnimate2 = ["myBar2", "myBar5"];
+  var elementsToAnimate3 = ["myBar1", "myBar3"];
+
+  elementsToAnimate1.forEach(function(elementId) {
+    move(elementId, 100);
+  });
+
+  elementsToAnimate2.forEach(function(elementId) {
+    move(elementId, 75);
+  });
+
+  elementsToAnimate3.forEach(function(elementId) {
+    move(elementId, 50);
+  });
+
+  createCircularProgress();
 });
